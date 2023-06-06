86fc5f83f45fdb72c4396b2779366082e2c5c22a
bhavadeep reddy <bhavadeep2003@gmail.com>
2023-06-06 16:48:11 -0700

    commit

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const app = express();
 const https = require("https");
-
-
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (req, res) => {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=7f49e937afaa46f39f19f3f4136c9bc8"
-   https.get(url,function(response){
+
+    res.sendFile(__dirname + '/index.html');
+   
+   
+   })
+    app.post("/",function(req, res){
+       
+        const query = req.body.cityName;
+        const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=7f49e937afaa46f39f19f3f4136c9bc8"
+        https.get(url,function(response){
     
     response.on("data",function(data){
         const weatherData = JSON.parse(data)
-        const temp = weatherData.main.temp
+       
        res.send("climate in mumbai is" + data +"this")
     })
-   })
-    
+     })
 })
 app.listen(9985 ,() => {
     console.log('port connected');
